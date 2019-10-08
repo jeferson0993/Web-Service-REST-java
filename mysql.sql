98f2ad999a214dbd434f3ffcca292c946a5e49a1
JefersonFerreira <jeferson0993@gmail.com>
2019-10-08 18:26:32 -0300

    Update mysql.sql
    Comentado a parte de insert dos dados

--- mysql.sql
+++ mysql.sql
@@ -9,6 +9,12 @@ CREATE DATABASE angularjs;
 
 use angularjs;
 
+CREATE TABLE IF NOT EXISTS `empresa` (
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `cnpj` VARCHAR(45) NULL,
+  `razaoSocial` VARCHAR(45) NULL,
+  `endereco` VARCHAR(45) NULL );
+
 CREATE TABLE IF NOT EXISTS `vaga` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `nome` VARCHAR(100) NOT NULL,
@@ -24,21 +30,6 @@ CREATE TABLE IF NOT EXISTS `vaga` (
   FOREIGN KEY (empresa_id) REFERENCES empresa(id));
    
 
-insert into vaga ( nome, especificacaoDoCargo, remuneracao, valeTransporte, valeRefeicao, outros, uf, turno, formaDeContratacao
-) values ( 'Frontend web developer jr', 'Desenvolvimento de aplicações web é mobile com integração de APIs restfull',
-'2738.00', true, true, 'Necessario experiências com angular, laravel, e bootstrap. Arquitetura MVC é um diferencial.',
-'TO', 'integral', 'clt' );
-
-insert into vaga ( nome, especificacaoDoCargo, remuneracao, valeTransporte, valeRefeicao, outros, uf, turno,
-formaDeContratacao ) values ( 'Backend web developer Sr', 'Desenvolvimento de APIs restfull com MongoBD e mysql',
-'4338.27', true, true, 'Necessario experiências com expressjs', 'SP', 'integral', 'pj' ); 
-
-CREATE TABLE IF NOT EXISTS `empresa` (
-  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `cnpj` VARCHAR(45) NULL,
-  `razaoSocial` VARCHAR(45) NULL,
-  `endereco` VARCHAR(45) NULL );
-
 CREATE TABLE IF NOT EXISTS `candidato` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `dataNascimento` VARCHAR(45) NULL,
@@ -52,5 +43,20 @@ CREATE TABLE IF NOT EXISTS `candidato` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `cargo` VARCHAR(45) NULL,
   `funcao` VARCHAR(45) NULL,
-  `empresa_id` INT NULL,
-  FOREIGN KEY (empresa_id) REFERENCES empresa(id));
+  `candidato_id` INT NULL,
+  FOREIGN KEY (candidato_id) REFERENCES candidato(id));
+
+                                              
+                                              
+/*
+
+insert into vaga ( nome, especificacaoDoCargo, remuneracao, valeTransporte, valeRefeicao, outros, uf, turno, formaDeContratacao
+) values ( 'Frontend web developer jr', 'Desenvolvimento de aplicações web é mobile com integração de APIs restfull',
+'2738.00', true, true, 'Necessario experiências com angular, laravel, e bootstrap. Arquitetura MVC é um diferencial.',
+'TO', 'integral', 'clt' );
+
+insert into vaga ( nome, especificacaoDoCargo, remuneracao, valeTransporte, valeRefeicao, outros, uf, turno,
+formaDeContratacao ) values ( 'Backend web developer Sr', 'Desenvolvimento de APIs restfull com MongoBD e mysql',
+'4338.27', true, true, 'Necessario experiências com expressjs', 'SP', 'integral', 'pj' ); 
+
+*/
